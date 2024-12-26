bd94a77d5795574e30477f6e4d9c8403c2888cfb
pasichmaria <pasichmaria@gmail.com>
2024-12-26 14:41:15 +0700

    env

--- src/index.ts
+++ src/index.ts
@@ -5,27 +5,30 @@ import FastifyFormBody from "@fastify/formbody";
 import FastifyJwt from "@fastify/jwt";
 import Fastify from "fastify";
 
-import {BalanceModule, BinanceModule, SendGridModule, TransactionModule, UsersModule} from "./module"
-import {PrismaClient} from "@prisma/client";
+import { BalanceModule, BinanceModule, SendGridModule, TransactionModule, UsersModule } from "./module";
+import { PrismaClient } from "@prisma/client";
 
 async function start() {
-
   const logger = pino({
-    level: 'info',
+    level: "info",
     transport: {
-      target: 'pino-pretty',
+      target: "pino-pretty",
       options: {
         colorize: true,
       },
     },
   });
+
   const prisma = new PrismaClient();
 
   const fastify = Fastify({
-    logger : true
-  })
+    logger: true,
+  });
 
-  const sendgrid = require("@sendgrid/mail")
+  const PORT = process.env.PORT || 3001
+  const HOST = process.env.HOST || "127.0.0.1";
+
+  const sendgrid = require("@sendgrid/mail");
 
   await fastify.register(FastifyJwt, {
     secret: process.env.JWT_SECRET as string,
@@ -37,19 +40,19 @@ async function start() {
       complete: true,
     },
   });
+
   await fastify.register(FastifyCookie, {});
   await fastify.register(FastifyFormBody);
-
   await fastify.register(FastifyCors, {
     origin: "http://localhost:3000",
     credentials: true,
-  })
+  });
 
-  const _usersModule = await UsersModule.init({fastify, prisma});
-  const _balanceModule = await BalanceModule.init({fastify, prisma});
-  const _transactionModule = await TransactionModule.init({fastify, prisma});
-  const _sendGridModule = await SendGridModule.init({fastify, prisma});
-  const _binanceController = await BinanceModule.init({fastify, prisma})
+  const _usersModule = await UsersModule.init({ fastify, prisma });
+  const _balanceModule = await BalanceModule.init({ fastify, prisma });
+  const _transactionModule = await TransactionModule.init({ fastify, prisma });
+  const _sendGridModule = await SendGridModule.init({ fastify, prisma });
+  const _binanceController = await BinanceModule.init({ fastify, prisma });
 
   fastify.setNotFoundHandler((_req, reply) => {
     return reply.send("Not Found");
@@ -59,20 +62,14 @@ async function start() {
     return reply.send("Hello World");
   });
 
-
-  fastify.listen(process.env.PORT as string, '0.0.0.0', function (err, address) {
-    if (err) {
-      fastify.log.error(err);
-      process.exit(1);
-    }
-    fastify.log.info(`Server listening on ${address}`);
-  })
+    fastify.listen(PORT, HOST, (err, address) => {
+        if (err) {
+        logger.error(err);
+        process.exit(1);
+        }
+        logger.info(`Server listening on ${address}`);
+    });
 }
 
 
 start();
-
-//
-// fastify.listen({port: 8000}, (err) => {
-//   if (err) throw err
-//
